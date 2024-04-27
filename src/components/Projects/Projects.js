57e2f13ccaee401fe44326662b038d86f537230c
Bilal Arshad <muhammadbilalarshad490@gmail.com>
2024-04-27 15:21:01 +0500

    added new github links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               isBlog={false}
               title="EPPDA"
               description="A hybrid system consisting of a civilian mobile app through which a civilian can signup/login to report crimes based on location. The request will be forwarded to Police station' s website according to the location of the crime. Now from there the crime will be forwarded to the nearest patrolling police officer's app. The police officer can give update reports that will be conveyed to Admin and Police station's website and from there they will be forwarded to civilian's app. Now if a civilian reports a crime by simple calling, the call will be made to the dispatcher who will then fill the crime form and forward it to police station and the rest of the process will be the same. There will be an admin website which will be responsible for creating, updating and deleting Dispatcher, Police station and Police officer's accounts. The statistics of received/solved crimes will be shown to the admin in the form of HeatMap and Graph. An ML module will be implemented in which we will train the models on the faces of wanted criminals. Now Police app will have a feature to implement this model by taking a pic of a suspect and then running it through the model to find similarities between the taken suspect pic and those already present in database."
-              ghLink="On a Private Repo"
+              ghLink="https://github.com/Muhammad-Bilal-Arshad"
              
             />
           </Col>
@@ -39,7 +39,7 @@ function Projects() {
               isBlog={false}
               title="MiddleEarth BookShop"
               description="A mobile app created for a bookshop owners. It keeps tracks of all the bookshop inventory including which books are sold and which are in stock. It also keeps track of the customers and their orders. It also has a feature to add new books to the inventory. It also has a feature to view all the sales and sales can be sorted on the basis of either dates or the name of books. To create this application I used React Native and Firebase. "
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              ghLink="https://github.com/Muhammad-Bilal-Arshad/Middle-Earth-Book-Shop"
               
             />
           </Col>
